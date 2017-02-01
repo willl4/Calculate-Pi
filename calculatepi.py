472e11d4279e39cefd08c82c764dc9561bd3f4fa
willl4 <williamlaycock@hanovernorwichschools.org>
2017-02-01 13:54:07 -0500

    Update calculatepi.py

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Will Laycock
+Credit: did myself
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
